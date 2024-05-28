e1f0ef4497116719e075670cf65e8e0fda39a6b3
Pierre Comte <p.s.comte@hotmail.fr>
2024-05-28 15:09:32 +0200

    montant onclick

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function goBack() {
   window.history.back();
 }
+function selectAmount(amount) {
+  localStorage.setItem("selectedAmount", amount);
+}
+
+function validateAmount() {
+  const selectedAmount = localStorage.getItem("selectedAmount");
+
+  if (selectedAmount) {
+    window.location.href = "Moyenpaiement.html";
+  } else {
+    alert("Veuillez sélectionner un montant.");
+  }
+}
