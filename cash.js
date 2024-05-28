cffe7bd77aa60d4fc576044e15809834611e8dc2
Pierre Comte <p.s.comte@hotmail.fr>
2024-05-28 16:50:17 +0200

    cash fonctionnel

--- cash.js
+++ cash.js
@@ -2,12 +2,10 @@ let selectedAmount = 0;
 let currentAmount = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Retrieve the selected amount and payment method from localStorage
   selectedAmount = parseInt(localStorage.getItem("selectedAmount"));
   const paymentMethod = localStorage.getItem("paymentMethod");
 
   if (selectedAmount && paymentMethod) {
-    // Display the selected amount and payment method on the page
     document.getElementById("selectedAmount").textContent = selectedAmount;
     document.getElementById("paymentMethod").textContent = paymentMethod;
   }
@@ -39,6 +37,12 @@ function updateProgressBar() {
   currentAmountElement.textContent = currentAmount;
 
   if (currentAmount >= selectedAmount) {
-    alert("Montant total atteint!");
+    showSpinnerAndRedirect();
   }
+}
+function showSpinnerAndRedirect() {
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(function () {
+    window.location.href = "recapsolde.html";
+  }, 3000);
 }
